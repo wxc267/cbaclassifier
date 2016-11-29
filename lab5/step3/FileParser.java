7e8106f0123b33ea6fcae74aa743be41fe426fd7
wxc267 <wxc267@gmail.com>
2016-11-29 15:52:22 -0500

    make and readme

--- lab5/step3/FileParser.java
+++ lab5/step3/FileParser.java
@@ -10,10 +10,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 
 public class FileParser {
